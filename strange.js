8e9d94bf5f9967ee26af4b36ecff63abb5187dc4
Matt <matt@supermatty.com>
2013-03-18 15:31:46 +0000

    Get this rendering. Doesn't look very cool yet (needs a better attrator, I suspect).
    git-svn-id: https://svn/svn/StrangeJavascript@2768 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- strange.js
+++ strange.js
@@ -3,6 +3,7 @@
 		var canvas = $('#theCanvas'),
 			context = canvas[0].getContext('2d'),
 			imageData = context.getImageData(0, 0, canvas[0].width, canvas[0].height),
+			scale = 40,
 			numPoints = 1000,
 			points = new RingBuffer(numPoints),
 			startStopButton = $('#startStopButton'),
@@ -11,13 +12,21 @@
 		// Begin with a point at (1, 1)
 		points.append({x: 1, y: 1});
 		function setPixel(imageData, x, y, r, g, b, a) {
-			var i = (y * imageData.width * 4) + (x * 4);
+			var i;
+			// Put the canvas origin in the centre and zoom in
+			x *= scale;
+			y *= scale;
+			x += canvas[0].width / 2;
+			y += canvas[0].height / 2;
+			x = Math.floor(x);
+			y = Math.floor(y);
+			i = (y * imageData.width * 4) + (x * 4);
 			imageData.data[i + 0] = r;
 			imageData.data[i + 1] = g;
 			imageData.data[i + 2] = b;
 			imageData.data[i + 3] = a;
 		}
-		function iterate(p) {
+		function iteratePoint(p) {
 			var params = { a: -0.89567065, b:  1.59095860, c:  1.8515863, d:  2.197430600 };
 			return {
 				x: Math.sin(params.a * p.y) - Math.cos(params.b * p.x),
@@ -26,7 +35,6 @@
 		}
 		function tick() {
 			var p, colour;
-			debug('tick');
 			if (points.isFull()) {
 				// Erase oldest point, which is about to be over-written
 				p = points.get(0);
@@ -35,13 +43,17 @@
 			// Retrieve current point
 			p = points.get(points.getLength() - 1);
 			// Generate colour of next point from current point's x co-ordinate
-			colour = hsv2rgb(p.x, 0.8, 1);
+			colour = hsv2rgb(360 * p.x, 0.8, 1);
+			// debug(p.x + ' => ' + colour);
 			// Generate co-ordinates of next point from those of current point
-			p = iterate(p);
+			p = iteratePoint(p);
 			// Add next point to list
 			points.append(p);
 			// Draw next point
-			setPixel(imageData, p.x, p.y, colour[0], colour[1], colour[2], 0);
+			setPixel(imageData, p.x, p.y, colour[0], colour[1], colour[2], 255);
+			// FIXME: Specifying a dirty rectangle like the below doesn't work in Chrome
+			// context.putImageData(imageData, 0, 0, p.x, p.y, 1, 1);
+			context.putImageData(imageData, 0, 0);
 			// Arrange for next tick call to be made as soon as possible.
 			// TODO: Apparently we can get true 0 timeouts by using window.postMessage.
 			if (keepRunning) { 
